f1d034dde3702147ecb186933eb852d88d456458
Kuuu <i.kudinov2@gmail.com>
2025-11-14 09:38:58 +0900

    Projects setup

---
+++ Common/netcommon.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <cstdint>
+#include "network.h"
+
+enum class GameMsg : uint32_t
+{
+	Server_GetStatus,
+	Server_GetPing,
+	Server_RespondDesignerRequest,
+	Server_ChangeParameter,
+
+	Client_Accepted,
+	Client_AssignID,
+	Client_RegisterWithServer,
+	Client_UnregisterWithServer,
+	Client_RequestDesigner,
+	Client_RequestChangeParameter,
+
+	Lobby_PlayerJoined,
+	Lobby_PlayerLeft,
+	Lobby_StartGame,
+
+	Game_AddPlayer,
+	Game_RemovePlayer,
+	Game_UpdatePlayer,
+};
+
+struct Vec2
+{
+	float x;
+	float y;
+};
+
+struct PlayerDescription
+{
+	uint32_t uniqueID = 0;
+	uint32_t ingameID = 0;
+	//uint32_t nAvatarID = 0;
+
+	Vec2 pos = {0.0f, 0.0f};
+	float radius = 0.0f;
+};
