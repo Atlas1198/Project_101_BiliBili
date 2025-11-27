c5bb37770b41945bbbe4c635309ecb1264b03027
北川歩 <ths40114@ths.hal.ac.jp>
2025-11-27 23:52:45 +0900

    MAPを作成しました。

--- Project_101_BiliBili/Wall.cpp
+++ Project_101_BiliBili/Wall.cpp
@@ -7,7 +7,7 @@ using namespace CollisionData;
 Wall::Wall(MeshData::MESH_TYPE meshType, DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 rotation, DirectX::XMFLOAT3 scale, DirectX::XMFLOAT3 velocity, bool isActive, ColliderType colliderType, DirectX::XMFLOAT3 collisionBoxSize, bool collisionIsTrigger)
 	: ObjectBase(meshType, position, rotation, scale, velocity, isActive, OBJECT_TAG::WALL, colliderType, COLLISION_LAYER::WALL, collisionBoxSize, collisionIsTrigger)
 {
-	m_isDrawn = false;
+	m_isDrawn = true;
 }
 
 //çXêV
