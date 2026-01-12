0d3c08f3e9d35db3befbb5165b4a847e1fd96c69
THAT12D18721 斎藤航希 <ths40857@ths.hal.ac.jp>
2026-01-12 19:37:33 +0900

    player

--- Project_101_BiliBili/PlayerManager.cpp
+++ Project_101_BiliBili/PlayerManager.cpp
@@ -265,7 +265,18 @@ void PlayerManager::PrepareRenderInfo(
 		&m_playerInfo,					//描画情報構造体配列へのポインタ
 		m_pPlayer[0]->GetMeshType(),	//メッシュタイプ
 		BLEND_MODE::BLEND_MASKED,		//ブレンドモード
-		L"asset/texture/white.png",	//テクスチャのファイル名
+		L"asset/texture/player/YELLOW_off_CH.png",		//テクスチャのファイル名
+		false,							//ライト無効
+		BILLBOARD_TYPE::BILLBOARD_SPHERICAL
+	);
+
+	CreateRenderInfo(
+		textureManager,					//テクスチャマネージャへの参照
+		meshManager,					//メッシュマネージャへの参照
+		&m_playerTransformInfo,			//描画情報構造体配列へのポインタ
+		m_pPlayer[0]->GetMeshType(),	//メッシュタイプ
+		BLEND_MODE::BLEND_MASKED,		//ブレンドモード
+		L"asset/texture/player/YELLOW_on_CH.png",		//テクスチャのファイル名
 		false,							//ライト無効
 		BILLBOARD_TYPE::BILLBOARD_SPHERICAL
 	);
