b302699a06cfe95df02952c1d17e00b9b8dd1baa
THAT12D18721 斎藤航希 <ths40857@ths.hal.ac.jp>
2026-02-02 18:03:17 +0900

    ok

--- Project_101_BiliBili/Spring.cpp
+++ Project_101_BiliBili/Spring.cpp
@@ -48,15 +48,21 @@ Spring::Spring(
         DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f)
     );
 
-    EventManager::GetInstance()->TriggerEvent<EffectCommand>(
-        EventType::ADD_EFFECT,
-        EffectCommand{
-            EFFECT_TYPE::WIND,
-            m_position,
-            XMFLOAT2{ 20.5f,20.5f },
-        }
-        );
+  //  XMFLOAT3 windPosition =
+  //  {
+  //      m_position.x,
+		//m_position.y +2.5f,
+		//m_position.z,
+  //  };
 
+  //  EventManager::GetInstance()->TriggerEvent<EffectCommand>(
+  //      EventType::ADD_EFFECT,
+  //      EffectCommand{
+  //          EFFECT_TYPE::WIND,
+  //          windPosition,
+  //          XMFLOAT2{ 4.0f,4.0f },
+  //      }
+  //      );
 }
 
 void Spring::SetLaunchTarget(const XMFLOAT3& target)
