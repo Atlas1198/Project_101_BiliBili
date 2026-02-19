e07f6f1f5eb3c75521524d04aa55147b7693d436
THAT12D18721 斎藤航希 <ths40857@ths.hal.ac.jp>
2026-02-19 09:57:22 +0900

    ok

--- Project_101_BiliBili/ResultUI.cpp
+++ Project_101_BiliBili/ResultUI.cpp
@@ -143,7 +143,7 @@ ResultUI::ResultUI(DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 scale, DirectX:
 			DirectX::XMFLOAT3{ 30.0f, 30.0f, 1.0f },		//スケール
 			DirectX::XMFLOAT3{ 0.0f, 0.0f, 0.0f },			//回転
 			100,												//描画順序
-			L"asset/texture/result_scene/UI_RESULT_Confetti.png",	//テクスチャパス
+			L"asset/texture/white.png",	//テクスチャパス
 			PSO_KEY_TRANSPARENT						//ブレンドモード
 		);
 		m_pConfettiImage[i]->SetActive(false); // 初期状態では非表示にする
