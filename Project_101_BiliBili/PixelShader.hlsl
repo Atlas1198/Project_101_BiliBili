c92573ee575c33afa9d0a4415844215a05d06fed
Kuuu <i.kudinov2@gmail.com>
2026-02-24 10:57:18 +0900

    maxUV fix

--- Project_101_BiliBili/PixelShader.hlsl
+++ Project_101_BiliBili/PixelShader.hlsl
@@ -69,7 +69,7 @@ float4 BasicPS(
     float2 offsetY = float2(0.0f, TexelSize.y * OutlineWidth);
     
     float2 minUV = uvRect.xy;
-    float2 maxUV = uvRect.xy + uvRect.zw;
+    float2 maxUV = uvRect.xy + uvRect.zw - TexelSize;
     
     // Calculate clamped UVs for the neighbors
     // clamp(value, min, max) ensures we never sample outside this sprite's box
