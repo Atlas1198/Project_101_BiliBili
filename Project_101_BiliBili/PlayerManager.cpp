c23ca2ba87f265ba0b5960ae9398a33fd01af667
THAT12D18721 斎藤航希 <ths40857@ths.hal.ac.jp>
2026-02-24 09:52:57 +0900

    ok

--- Project_101_BiliBili/PlayerManager.cpp
+++ Project_101_BiliBili/PlayerManager.cpp
@@ -227,12 +227,14 @@ void PlayerManager::OnTakeDamage(int teamID, float damage)
 		{//両方とも同時に被弾している場合は先にダメージアニメーションに入った方を被弾プレイヤーとする
 			FrameTimer player1DamageTimer = loserP1->GetDamageAnimTimer();
 			FrameTimer player2DamageTimer = loserP2->GetDamageAnimTimer();
-			lastDamagedPlayerIndex = (player1DamageTimer.Peek() >= player2DamageTimer.Peek()) ? p1Index : p2Index;
+			lastDamagedPlayerIndex = (player1DamageTimer.Peek() <= player2DamageTimer.Peek()) ? p1Index : p2Index;
+			OutputDebugStringA("Both players are damaged\n");
 		}
 		else
 		{
 			lastDamagedPlayerIndex = p1Damaged ? p1Index : p2Index;
 		}
+		OutputDebugStringA(("Team " + std::to_string(teamID) + " is defeated! Last damaged player index: " + std::to_string(lastDamagedPlayerIndex) + "\n").c_str());
 		m_pPlayer[lastDamagedPlayerIndex]->GetDamageAnimTimer().Mark();
 		EventManager::GetInstance()->TriggerEvent<std::tuple<bool, int, int, int, int>>(EventType::GAME_OVER, std::make_tuple(true, winningTeamID, winnerCharacter1ID, winnerCharacter2ID, lastDamagedPlayerIndex));
 	}
